8240b57cb49861937163f02d91695f9942467c49
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-10-04 13:57:46 +0200

    rsync via find working

--- rsyncify.sh
+++ rsyncify.sh
@@ -23,27 +23,9 @@ for p in ${patterns[@]}; do
 	regex="$regex$p|"
 done
 regex="(${regex::-1})"
-files=($(fd --type f "$regex" .))
 
-# on server side, detect changes and run cmd
-#   - possibly just watch a .sync file?
+# while sleep 2; do
+	cmd=$(echo "fd --type f \"$regex\" . | rsync --relative --verbose --files-from=- . $HOST:$REMOTE_DIR")
+	fd --type f "$regex" . | entr -ds "$cmd"
+# done
 
-function min() {
-	echo $(($1>$2 ? $2 : $1))
-}
-
-function dispatch_watcher() {
-	echo $@ | entr -rc "rsync"
-}
-
-n_files=${#files[@]}
-n_watchers=$(min 4 $n_files)
-chunk_size=$(($n_files / $n_watchers))
-
-for i in $(seq 0 $(($n_watchers-2))); do
-	start=$(($i * $chunk_size))
-	dispatch_watcher "${files[@]:$start:$chunk_size}"
-done
-# last group takes every file till the end
-start=$(($i * $chunk_size + $chunk_size))
-dispatch_watcher "${files[@]:$start}"
