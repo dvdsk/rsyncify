c7af8d0bbcdb8a4416161d0f5c03e193a3537096
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-10-04 14:31:56 +0200

    use controlpath to share conn

--- rsyncify.sh
+++ rsyncify.sh
@@ -24,8 +24,33 @@ for p in ${patterns[@]}; do
 done
 regex="(${regex::-1})"
 
+# set up a persistant ssh connection for rsync to use so:
+# - no need to wait for rsync to (re)connect whenever its used
+# - only need to authenticate once, no password prompts beyond here
+mkdir -p ~/.ssh/ctl
+# n: stdin from /dev/null, N: do not execute remote cmd, f: go to background
+ctrl_path="$HOME/.ssh/%L-%r@%h:%p" 
+ssh -nNf \
+	-o ControlPersist=yes \
+	-o ControlMaster=yes \
+	-o ControlPath=$ctrl_path \
+	$HOST
+
+ssh_cmd="ssh -o ControlPath=$ctrl_path"
+# echo "$ssh_cmd"
+# cmd=$(echo "fd --type f \"$regex\" . | rsync \
+# 	--rsh="$ssh_cmd" \
+# 	--relative \
+# 	--verbose \
+# 	--files-from=- . $HOST:$REMOTE_DIR")
+
+fd --type f \"$regex\" . | rsync \
+	--rsh="$ssh_cmd" \
+	--relative \
+	--verbose \
+	--files-from=- . $HOST:$REMOTE_DIR
+
 # while sleep 2; do
-	cmd=$(echo "fd --type f \"$regex\" . | rsync --relative --verbose --files-from=- . $HOST:$REMOTE_DIR")
-	fd --type f "$regex" . | entr -ds "$cmd"
+# 	fd --type f "$regex" . | entr -ds "$cmd"
 # done
 
